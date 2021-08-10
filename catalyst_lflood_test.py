5145f9cbdc55cf21e0a52daf68f7effff4b90e0a
Gian-Carlo DeFazio <defazio1@llnl.gov>
2021-08-10 15:11:41 -0700

    added retry for when slurm kicks me out

--- catalyst_lflood_test.py
+++ catalyst_lflood_test.py
@@ -11,71 +11,112 @@ import pprint
 import random
 import subprocess
 import sys
+import time
 
 import yaml
 
+
+# TODO make this not suck
+# have arg parser, be able to specify a bunch of tests
+# and have it just go
+# record stuff in resonable, searchable way
+# pull the restuls of out the logs
+
+# be able to read in config file for:
+# machine it's on
+# ior and mdtest params
+# batch to use
+# number of node, and processes per node
+
+# TODO see why HOSTNAME fails for root
+
+
 # TODO make it so script can run on opal
 
+# maybe get rid of globals, just have a dict
+# that get set by get_config, or has defaults
 
-ior_logs_dir = pathlib.Path(
-    '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/ior_logs'
-)
-mdtest_logs_dir = pathlib.Path(
-    '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/mdtest_logs'
-)
-scripts_dir = pathlib.Path(
-    '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/srun_commands'
-)
-stonewall_status = pathlib.Path(
-    '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/stonewall_status'
-)
+# the cameron values might get renamed or moved
+# kept here for now
+# cameron_mdtest_create_flags = [
+#     str(mdtest_path),
+#     '-n', '1000000', '-u', '-L', '-F', '-P', '-N', '1',
+#     '-d', str(mdtest_files_path),
+#     '-x', str(stonewall_status),
+#     '-C', '-Y', '-W', '300', '-a', 'POSIX',
+# ]
+# cameron_mdtest_stat_flags = [
+#     str(mdtest_path),
+#     '-n', '1000000', '-u', '-L', '-F', '-P', '-N', '1',
+#     '-d', str(mdtest_files_path),
+#     '-x', str(stonewall_status),
+#     '-T', '-a', 'POSIX',
+# ]
 
-#if 'catalyst' in os.environ['HOSTNAME']:
-mdtest_files_path = '/p/lflood/defazio1/io500-all/mdtest-easy-cleanup'
-ior_files_path = ''
-partition = ['-p', 'pgarter']
-#elif 'opal' in os.environ['HOSTNAME']:
-#    mdtest_files_path = '/p/lquake/defazio1/io500-all'
-#    ior_files_path = ''
-#    partition = []
 
-mdtest_path = pathlib.Path('/g/g0/defazio1/repos/ior/src/mdtest')
 
-ior_path = pathlib.Path('/g/g0/defazio1/repos/ior/src/ior')
+def get_hostname():
+    '''For some reason root does have HOSTNAME in env,
+    so you the hostname utility instead.
+    '''
+    return subprocess.run(
+        ['hostname'],
+        check=True,
+        stdout=subprocess.PIPE,
+    ).stdout.decode().strip()
 
-cameron_mdtest_create_flags = [
-    str(mdtest_path),
-    '-n', '1000000', '-u', '-L', '-F', '-P', '-N', '1',
-    '-d', str(mdtest_files_path),
-    '-x', str(stonewall_status),
-    '-C', '-Y', '-W', '300', '-a', 'POSIX',
-]
+def get_config(config_file_path):
+    '''Read in a config file, and set the globals to it
+    values.
+    '''
+    with open(config_file_path, 'r') as f:
+        return yaml.safe_load(f)
 
-cameron_mdtest_stat_flags = [
-    str(mdtest_path),
-    '-n', '1000000', '-u', '-L', '-F', '-P', '-N', '1',
-    '-d', str(mdtest_files_path),
-    '-x', str(stonewall_status),
-    '-T', '-a', 'POSIX',
-]
+# set some globals, and like all good globlas, these should not be modified
+# ior_logs_dir = pathlib.Path(
+#     '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/ior_logs'
+# )
+# mdtest_logs_dir = pathlib.Path(
+#     '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/mdtest_logs'
+# )
+# scripts_dir = pathlib.Path(
+#     '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/srun_commands'
+# )
+# stonewall_status = pathlib.Path(
+#     '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/stonewall_status'
+# )
 
-def make_mdtest_command_from_template(timestamp, template):
+# if 'catalyst' in get_hostname():
+#     mdtest_files_path = '/p/lflood/defazio1/io500-all/mdtest-easy-cleanup'
+#     ior_files_path = ''
+#     partition = ['-p', 'pgarter']
+# elif 'opal' in get_hostname():
+#     mdtest_files_path = '/p/lquake/defazio1/io500-all'
+#     ior_files_path = ''
+#     partition = []
+
+# mdtest_path = pathlib.Path('/g/g0/defazio1/repos/ior/src/mdtest')
+
+# ior_path = pathlib.Path('/g/g0/defazio1/repos/ior/src/ior')
+
+
+def make_mdtest_command_from_template(timestamp, template, config):
     '''Make a command that can be run and will store it's data in the
     right place. The template can omit the location of where the test files
     will be created (after flag -d) and where the stonewall logs will go
     (after flag -x)
     '''
     command = copy.deepcopy(template)
-    command.insert(0, str(mdtest_path))
-    command.insert(str(mdtest_files_path), command.index('-d')+1)
-    command.insert(str(mdtest_files_path / timestamp), command.index('-x')+1)
+    command.insert(0, str(config['mdtest_path']))
+    command.insert(str(config['mdtest_files_path']), command.index('-d')+1)
+    command.insert(str(config['mdtest_files_path'] / timestamp), command.index('-x')+1)
 
     return command
 
-def make_mdtest_command(timestamp, command=None):
+def make_mdtest_command(timestamp, config, command=None):
     if command is None:
         command = [
-            str(mdtest_path),
+            str(config['mdtest_path']),
             '-v', # verbose
             # TODO should be -n 1000000
             '-n', '100', # number of files per process
@@ -84,9 +125,9 @@ def make_mdtest_command(timestamp, command=None):
             '-F',
             '-P',
             '-N', '1',
-            '-d', str(mdtest_files_path),
+            '-d', str(config['mdtest_files_path']),
             #'-x', '/g/g0/defazio1/mdtest_results/mdtest-easy-cleanup.stonewall',
-            '-x', str(stonewall_status / timestamp),
+            '-x', str(config['stonewall_status'] / timestamp),
             '-Y',
             '-W', '300',
             '-a', 'POSIX',
@@ -97,10 +138,10 @@ def make_mdtest_command(timestamp, command=None):
     command = ' '.join(command)
     return command
 
-def make_mdtest_command(timestamp, command=None):
+def make_mdtest_command(timestamp, config, command=None):
     if command is None:
         command = [
-            str(mdtest_path),
+            str(config['mdtest_path']),
             '-v',
             '-n', '1000000',
             '-u',
@@ -108,8 +149,8 @@ def make_mdtest_command(timestamp, command=None):
             '-F',
             '-P',
             '-N', '1',
-            '-d', str(mdtest_files_path),
-            '-x', str(stonewall_status / timestamp),
+            '-d', str(config['mdtest_files_path']),
+            '-x', str(config['stonewall_status'] / timestamp),
             '-T',
             '-C',
             '-Y',
@@ -122,9 +163,9 @@ def make_mdtest_command(timestamp, command=None):
 
 
 
-def make_ior_command(timestamp):
+def make_ior_command(timestamp, config):
     command = [
-        str(ior_path),
+        str(config['ior_path']),
         '-C',
         '-Q', '1',
         '-g',
@@ -142,20 +183,20 @@ def make_ior_command(timestamp):
         '-a', 'POSIX',
     ]
 
-def make_srun_command(test_command_path, num_nodes=None, num_procs=None): # , nodes_and_procs):
+def make_srun_command(test_command_path, config, num_nodes=None, num_procs=None): # , nodes_and_procs):
     #num_nodes, num_procs = get_nodes_and_procs()
     # pgarter is specifically for catalyst
     #num_nodes, num_procs = 4,4
 
-    cmd = ['srun'] + partition + [f'-N{num_nodes}',
+    cmd = ['srun'] + config['partition'] + [f'-N{num_nodes}',
                                   f'-n{num_procs}',
                                   '-l',
                                   str(test_command_path)]
     return cmd
 
 
-def single_srun(test_type='mdtest', jbod_zfs_params=None, dryrun=False,
-                num_nodes=None, num_procs=None):
+def single_srun(config, test_type='mdtest', jbod_zfs_params=None, dryrun=False,
+                num_nodes=None, num_procs=None, tries=5):
     '''Create a file with a ior or mdtest command, then run it.
     Test type is either 'mdtest' or 'ior'
     jbod_zfs_params is a string of the jbod and zfs commands
@@ -165,10 +206,10 @@ def single_srun(test_type='mdtest', jbod_zfs_params=None, dryrun=False,
 
     if test_type.lower() == 'mdtest':
         make_command = make_mdtest_command
-        logs_dir = mdtest_logs_dir
+        logs_dir = config['mdtest_logs_dir']
     elif test_type.lower() == 'ior':
         make_command = make_ior_test
-        logs_dir = ior_logs_dir
+        logs_dir = config['ior_logs_dir']
 
 
     #optional_flags = get_optional_flags(all_optional_flags)
@@ -176,9 +217,10 @@ def single_srun(test_type='mdtest', jbod_zfs_params=None, dryrun=False,
 
     command = make_command(
         timestamp,
+        config,
         #optional_flags=optional_flags
     )
-    script_path = scripts_dir / timestamp
+    script_path = config['scripts_dir'] / timestamp
     with open(script_path, 'w') as f:
         if dryrun:
             f.write('# DRYRUN, this script will not be executed')
@@ -190,6 +232,7 @@ def single_srun(test_type='mdtest', jbod_zfs_params=None, dryrun=False,
             f.write('# ' + str(jbod_zfs_params) + '\n')
     script_path.chmod(0o755)
     srun_command = make_srun_command(script_path,
+                                     config,
                                      num_nodes=num_nodes,
                                      num_procs=num_procs)
     output_logs_path = logs_dir / timestamp
@@ -204,16 +247,79 @@ def single_srun(test_type='mdtest', jbod_zfs_params=None, dryrun=False,
     print(run_data)
     print()
     if not dryrun:
-        srun_output = subprocess.run(
-            srun_command,
-            check=True,
-            stdout=subprocess.PIPE,
-            #stderr=subprocess.STDOUT,
-        ).stdout.decode()
+
+        # slurm keeps kicking me out, so retry as needed
+        for t in range(1,tries+1):
+            try:
+                srun_output = subprocess.run(
+                    srun_command,
+                    check=True,
+                    stdout=subprocess.PIPE,
+                    #stderr=subprocess.STDOUT,
+                ).stdout.decode()
+                break
+            except CalledProcessError:
+                print('failed try {t} of {tries}, retrying in 1 minute...')
+                time.sleep(60)
+                print('trying again')
+
         with open(output_logs_path, 'w+') as f:
             f.write(srun_output)
 
 # /g/g0/defazio1/ior/src/mdtest '-v' '-n' '1000000' '-u' '-L' '-F' '-P' '-N' '1' '-d' '/p/lflood/defazio1/io500-all/mdtest-easy-cleanup' '-x' '/g/g0/defazio1/mdtest_results/mdtest-easy-cleanup.stonewall' '-Y' '-W' '300' '-a' 'POSIX'
+def get_config_values(args):
+    '''Get the configuration values. These come from either the defaults
+    or from a config file. Also, some can be specified on the command
+    line, but that happens external to this function.
+    '''
+    # get the default values
+    defaults = {}
+
+    # set the hostname, which is used for other value
+    defaults['hostname'] = get_hostname()
+
+    defaults['ior_logs_dir'] = pathlib.Path(
+        '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/ior_logs'
+    )
+    defaults['mdtest_logs_dir'] = pathlib.Path(
+        '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/mdtest_logs'
+    )
+    defaults['scripts_dir'] = pathlib.Path(
+        '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/srun_commands'
+    )
+    defaults['stonewall_status'] = pathlib.Path(
+        '/g/g0/defazio1/non-jira-projects/jbod-zfs-asp-tests/stonewall_status'
+    )
+
+    if 'catalyst' in defaults['hostname']:
+        defaults['mdtest_files_path'] = '/p/lflood/defazio1/io500-all/mdtest-easy-cleanup'
+        defaults['ior_files_path'] = ''
+        defaults['partition'] = ['-p', 'pgarter']
+    elif 'opal' in defaults['hostname']:
+        defaults['mdtest_files_path'] = '/p/lquake/defazio1/io500-all'
+        defaults['ior_files_path'] = ''
+        defaults['partition'] = []
+
+    defaults['mdtest_path'] = pathlib.Path(
+        '/g/g0/defazio1/repos/ior/src/mdtest'
+    )
+
+    defaults['ior_path'] = pathlib.Path(
+        '/g/g0/defazio1/repos/ior/src/ior'
+    )
+
+    config_file_path = args.get('config')
+    if config_file_path is not None:
+        defaults.update(get_config())
+
+    args_filtered = {k:v for k,v in args.items() if k in defaults}
+    # add any matching keys from args
+
+
+    # get the value from the config file and update
+    defaults.update(args_filtered)
+
+    return defaults
 
 def make_parser():
     description = 'do some tests'
@@ -260,32 +366,41 @@ def make_parser():
     return parser
 
 
+#def iter_nodes_procs()
+
+
 
 def main():
     parser = make_parser()
     args = vars(parser.parse_args())
+    config = get_config_values(args)
+
+    #print(config)
+    #sys.exit(1)
     print(args)
 
 
     if args['iterate']:
-        for num_nodes_base in range(6):
-            for procs_per_node_base in range(5):
-                # hack to restart at the right spot
-                #if num_nodes_base == 0 and procs_per_node_base <=3:
-                #    pass
-                #else:
-                num_nodes = 2**num_nodes_base
-                procs_per_node =2** procs_per_node_base
+        for num_nodes in [8,16,32]:
+            for procs_per_node in [1,2,4,8,16]:
+                if num_nodes == 8 and procs_per_node < 2:
+                    continue
+
+                #num_nodes = 2**num_nodes_base
+                #procs_per_node =2** procs_per_node_base
                 num_procs = num_nodes * procs_per_node
                 single_srun(
+                    config,
                     dryrun=args['dryrun'],
                     jbod_zfs_params=args['zfs_params'],
                     num_nodes=num_nodes,
                     num_procs=num_procs,
                 )
-
     else:
-        single_srun(dryrun=args['dryrun'], jbod_zfs_params=args['zfs_params'])
+        single_srun(
+            dryrun=args['dryrun'],
+            jbod_zfs_params=args['zfs_params']
+        )
 
 
 
--- summarize.py
+++ summarize.py
@@ -6,6 +6,8 @@ import re
 zfs_line_pattern = r'^# {.*}$'
 zfs_line_pattern = re.compile(zfs_line_pattern)
 
+
+
 # TODO store the zfs params in a better way to avoid ad-hoc parsing
 def parse_zfs_params(path):
     '''Given the path to a file,
@@ -28,7 +30,42 @@ def parse_zfs_params(path):
     # now interpret it into a dict
     return ast.literal_eval(dict_part)
 
-def get_data_from_mdtest():
+def get_data_from_mdtest(mdtest_logs_path):
     '''Parse the output of mdtest and get the data
     you crave!
     '''
+
+    def grab_tokens(line):
+        tokens = line.split()
+        names = ('max', 'min', 'mean', 'stddev')
+        return {n: t for n,t in zip(names, tokens[3:])}
+
+
+
+    with open(mdtest_logs_path, 'r') as f:
+        lines = f.read().splitlines()
+
+    # find where the actual summary data starts
+    # There are two sections "SUMMARY rate" and "SUMMARY time"
+    state = 'neither'
+    stats = {'rate': {}, 'time': {}}
+    summary_vals = [
+        'File creation',
+        'File stat',
+        'File read',
+        'File removal',
+        'Tree creation',
+        'Tree removal',
+    ]
+    for line in lines:
+        if 'SUMMARY rate' in line:
+            state = 'rate'
+        elif 'SUMMARY time' in line:
+            state = 'time'
+
+        for val in summary_vals:
+            if val in line and state in ('rate', 'time'):
+                val_snake = val.lower().replace(' ', '_')
+                stats[state][val_snake] = grab_tokens(line)
+
+    return stats
